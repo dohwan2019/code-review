f69c2542b62d35f04b945f15cdaee2d03f5169aa
dohwan <goodkandy@naver.com>
2019-01-29 17:04:12 +0900

    ffff

--- projects/Todos.jsx
+++ projects/Todos.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import './Todos.css';
 
 const Todos = (props) => {
-  return (
-    <div className="Todos">
-      <h1>{props.title}</h1>
-      <ul>
-        {props.items.map((item, idx)=>{
-          return (
-            <li key={idx}><input type="checkbox"/>{item}</li>
-          )
-        })}
-      </ul>
-    </div>
-  )
+    return (
+        <div className="Todos">
+            <h1>{props.title}</h1>
+            <ul>
+                {props.items.map((item, idx)=>{
+                    return (
+                        <li key={idx}>{item.name} -> {item.completed?"완료":"미완료"}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default Todos;
