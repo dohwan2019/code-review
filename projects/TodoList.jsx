83d2d2e7d1978bbf1580b4654444b1daea562573
dohwan <goodkandy@naver.com>
2019-01-30 14:14:07 +0900

    todos 2019-01-30 14:13

---
+++ projects/TodoList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './TodoList.css';
+
+class TodoList extends React.Component {
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  render() {
+    const {title, items, isUnmount} = this.props;
+    console.log({isUnmount});
+    return (
+      <div className="Todos">
+        <h1>{title}</h1>
+        <ul>
+          {items.map((item, idx)=>{
+            return (
+              <li key={idx}><input type="checkbox" checked={isUnmount?"checked":""} readOnly/>{item.name} -> {isUnmount?"완료":"미완료"}</li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default TodoList;
